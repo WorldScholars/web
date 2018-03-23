53d543422e59793b3acfb074590fc063d4f9160f
Mark Santolucito <mark.santolucito@yale.edu>
2018-03-23 12:00:04 -0400

    minor wording

--- src/Pages/Programs/SATPlus2018.hs
+++ src/Pages/Programs/SATPlus2018.hs
@@ -24,7 +24,7 @@ sat2018 = Program{
  ,dates = "Aug 2018"
  ,epoch = 1534529026 
  ,summary = do
-   p "This year World Scholars will bring its SAT+ program back to the community, condensed into a two week program from August 6-17. This intensive SAT prep program goes beyond just preparing students for standardized testing, but helps hone their academic and interpersonal skills to prepare them for the college admissions process."
+   p "This year World Scholars will bring its SAT+ program back to the community, condensed into a two week program from August 6-17. This intensive SAT prep program goes beyond just preparing students for standardized testing, but helps hone their academic and interpersonal skills to prepare them for the college admissions process. The program is best suited for student in grades 9-12."
  ,mainPic = "images/home/class.jpg"
  ,captionedPics = []
  ,extraHTML = do
@@ -45,7 +45,7 @@ sat2018 = Program{
             ,["4:30 PM", "Students picked up"]] :: [[String]])
    p "What is a Passion Project? In America and around the globe, educators are moving away from test-centric summative evaluations for learning. The educational landscape is moving towards a formative assessment in which students are expressing their learning in a more comprehensive way through projects. Project-based learning is the inspiration behind Passion Projects. We value the multi-dimensional ways of expressing learning through topics that students care about and we want to make sure that we provide an opportunity for students to make that happen. The Passion Projects will allow students to collaborate, innovate, and design a project (whether it be an idea or a product) of their choosing and we will facilitate to see through the project's completion.  Guest lecturers will come on Tuesdays and Thursdays to expose students to potential academic and professional career paths as well as inspire Passion Project work."
    p "By participating in the World Scholars program, students will develop the skills to present themselves, not just as strong students, but as future leaders. The final project gives students the first push to take charge of their own education and find passion in their work. Not all students will continue to work on their Passion Project after the program, but all students will use those skills to find the next Passion Project to work on."
-   p "To participate in the next SAT+ program and start your own Passion Project, stay tuned for registration instructions. The enrollment for this program is capped at 20 students per week and positions in our program are awarded on a first come, first serve basis."
+   p "To participate in the next SAT+ program and start your own Passion Project, stay tuned for registration instructions. Enrollment for this program is capped at 20 student per week per section, and we run two sections simultaneously in both weeks."
    p "Price and location TBA"
    a ! href "mailto:fiona@worldscholars.global" $ "fiona@worldscholars.global"
 }
