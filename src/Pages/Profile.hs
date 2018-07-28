d49e859a9ce75da41e7c348646201e657f17ad03
Mark Santolucito <mark.santolucito@yale.edu>
2018-07-28 08:11:03 -0400

    autocommit

---
+++ src/Pages/Profile.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Pages.Profile (profile) where
+
+import Prelude
+import qualified Prelude as P
+import Data.Monoid (mempty)
+import Templates.Scripts
+
+import Text.Blaze
+import Text.Blaze.Html5
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5.Attributes as A
+
+profile :: Html
+profile = do
+    section ! A.id "Profile" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      h1 ! class_ "margin-bottom" $ "Sign up for an account on World Scholars"
+      p ! A.id "username" $ ""
+
+      awsScripts
+
---
+++ src/Pages/SignInUp.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Pages.SignInUp (signup,verify,signin) where
+
+import Prelude
+import qualified Prelude as P
+import Data.Monoid (mempty)
+import Templates.Scripts
+
+import Text.Blaze
+import Text.Blaze.Html5
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5.Attributes as A
+
+signup :: Html
+signup = do
+    section ! A.id "signup" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      h1 ! class_ "margin-bottom" $ "Sign up for an account on World Scholars"
+      section ! class_ "form-wrap" $ do
+        h1 "Register"
+        H.form ! A.id "registrationForm" $ do
+          input ! type_ "email" ! A.id "emailInputRegister" ! placeholder "Email" ! pattern ".*" ! required ""
+          input ! type_ "password" ! A.id "passwordInputRegister" ! placeholder "Password" ! pattern ".*" ! required ""
+          input ! type_ "password" ! A.id "password2InputRegister" ! placeholder "Confirm Password" ! pattern ".*" ! required ""
+          input ! type_ "submit" ! value "Sign up"
+      awsScripts
+
+signin :: Html
+signin = do
+    section ! A.id "signin" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      section ! class_ "form-wrap" $ do
+        h1 "Sign in"
+        H.form ! A.id "signinForm" $ do
+          input ! type_ "email" ! A.id "emailInputSignin" ! placeholder "Email" ! required ""
+          input ! type_ "password" ! A.id "passwordInputSignin" ! placeholder "Password" ! pattern ".*" ! required ""
+          input ! type_ "submit" ! value "Sign In"
+      awsScripts
+
+
+verify :: Html
+verify = do
+    section ! A.id "verify" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      section ! class_ "form-wrap" $ do
+        h1 "Verify Email"
+        H.form ! A.id "verifyForm" $ do
+          input ! type_ "email" ! A.id "emailInputVerify" ! placeholder "Email" ! required ""
+          input ! type_ "text" ! A.id "codeInputVerify" ! placeholder "Verification Code" ! pattern ".*" ! required ""
+          input ! type_ "submit" ! value "Verify"
+      awsScripts
+
