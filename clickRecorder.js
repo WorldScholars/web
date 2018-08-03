dc2af3a02253b05d93d0a5651afb2f25b5c54406
Mark Santolucito <mark.santolucito@yale.edu>
2018-08-03 19:39:34 -0400

    autocommit

--- clickRecorder.js
+++ clickRecorder.js
@@ -13,6 +13,12 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  )
+}
+
 $('#scantronForm input').click(function () {
   //Localstorage only stores strings, so we convert between json
   var clicks = JSON.parse(localStorage.getItem('clicks')) || [] ;
@@ -62,6 +68,7 @@ $('#scantronForm input').click(function () {
                 Authorization: authToken
             },
             data: JSON.stringify({
+               examId: uuidv4(),
                examNumber: getUrlParameter('tNum'),
                clicks: JSON.parse(localStorage.getItem('clicks')),
                examAnswers : $('#scantronForm').serializeArray()
