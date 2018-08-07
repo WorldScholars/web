1f46d27577ac855b6d3dab9c7d42efe2e3d859e1
Mark Santolucito <mark.santolucito@yale.edu>
2018-08-07 00:07:28 -0400

    autocommit

--- src/Pages/Interactive/TeacherPortal.hs
+++ src/Pages/Interactive/TeacherPortal.hs
@@ -16,8 +16,9 @@ import qualified Text.Blaze.Html5.Attributes as A
 teacherPortal :: Html
 teacherPortal = do
     section ! A.id "Profile" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
-      h1 ! A.id "teacherWelcome" $ "Welcome teacher "
-      h2 ! A.id "whichExam" $ "Showing results (# wrong) for Exam #"
+      h1 ! A.id "teacherWelcome" $ "Welcome "
+      h2 ! A.id "whichExam" $ "Exam #"
+      h2 $ "Personalized summaries for each student"
       table ! A.class_ "answer-table" $ do
           thead $ tr $ do
               th "Student"
@@ -33,6 +34,7 @@ teacherPortal = do
               th "Top Problem Area 5"
               th "Top Problem Area 6"
           tbody mempty
+      h2 "Collected results over all students, sorted in order of most wrong"
       H.div ! A.id "tables" $ ""
       p ! A.id "summary" $ ""
 
