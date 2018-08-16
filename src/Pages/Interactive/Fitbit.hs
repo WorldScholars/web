8c8bb37b23ac7595f2c3f3772b57ecb5bd84a16c
Mark Santolucito <mark.santolucito@yale.edu>
2018-08-15 20:46:18 -0400

    autocommit

---
+++ src/Pages/Interactive/Fitbit.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Pages.Interactive.Fitbit (fitbitAuthResponse) where
+
+import Prelude
+import qualified Prelude as P
+import Data.Monoid (mempty)
+import Templates.Scripts
+
+import Text.Blaze
+import Text.Blaze.Html5
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes
+import qualified Text.Blaze.Html5.Attributes as A
+
+fitbitAuthResponse :: Html
+fitbitAuthResponse = do
+    section ! A.id "signup" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      h1 "saving your fitbit auth credentials to the database... This allows us to make cool graphs for you"
+      h1 "PLEASE DON'T NAVIGATE AWAY FROM THIS PAGE"
+      h1 "BE PATIENT WITH ME - I STRUGGLE SOMETIMES"
