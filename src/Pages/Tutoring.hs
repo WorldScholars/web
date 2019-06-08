a196690ad60eb5bdfcb10b850d1f1b2847c5bfa2
Mark Santolucito <mark.santolucito@yale.edu>
2019-06-07 21:43:14 -0400

    autocommit

---
+++ src/Pages/Tutoring.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pages.Tutoring (tutoringRegistration) where
+
+import Text.Blaze.Html5 hiding (map,head,summary)
+import qualified Text.Blaze.Html5 as H hiding (head,summary)
+import Text.Blaze.Internal (AttributeValue)
+import Data.String
+import qualified Data.List as L
+import Text.Blaze.Html5.Attributes hiding (name,summary)
+import qualified Text.Blaze.Html5.Attributes as A hiding (name,summary)
+
+import Text.Blaze.Html5 (table, td, tr, toHtml, ToMarkup, Html)
+import Control.Monad (forM_, mapM_)
+
+import Templates.Blocks 
+
+
+tutoringRegistration:: String -> Html
+tutoringRegistration buttonHTML = do
+    section ! A.id "about-company" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ H.div ! class_ "col-sm-12 text-center" $ do
+      h1 ! class_ "margin-bottom" $ "Tutoring"
+      p "World Scholars is excited to be offering tutoring on a personal referal basis."
+    section ! A.id "payment-options-check" ! class_ "wow fadeInUp" ! dataAttribute "wow-duration" "400ms" $ H.div ! class_ "container" $ H.div ! class_ "row" $ do
+      H.div ! class_ "col-sm-6 text-center" $ do
+        h2 "We are currently only accepting Paypal"
+        preEscapedToHtml $ buttonHTML
+
