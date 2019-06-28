6cb8458c6a4ad89a3e68f57f61db55f7ad0cab25
Mark Santolucito <mark.santolucito@yale.edu>
2019-06-28 19:37:13 -0400

    add interactive structure

---
+++ src/Pages/Interactive.hs
@@ -0,0 +1,16 @@
+module Pages.Interactive (
+     module Pages.Interactive.SignInUp
+   , module Pages.Interactive.Profile
+   , module Pages.Interactive.Scantron
+   , module Pages.Interactive.ExamSummary
+   , module Pages.Interactive.TeacherPortal
+   , module Pages.Interactive.Fitbit
+   ) where
+
+import Pages.Interactive.SignInUp
+import Pages.Interactive.Profile
+import Pages.Interactive.Scantron
+import Pages.Interactive.ExamSummary
+import Pages.Interactive.TeacherPortal
+import Pages.Interactive.Fitbit
+
