9e23aff538c065f8890b9bd5b45763aaf6b9b668
Mark Santolucito <mark.santolucito@yale.edu>
2019-06-28 20:47:09 -0400

    autocommit

--- custom_js/examReader.js
+++ custom_js/examReader.js
@@ -81,8 +81,8 @@
 
         console.log(correctBySection)
         finalScore = score(
-                      correctBySection["3"] + correctBySection["4"],
-                      correctBySection["1"], correctBySection["2"])
+                      correctBySection["3"] + correctBySection["4"] || 0,
+                      correctBySection["1"] || 0, correctBySection["2"] || 0)
         console.log(finalScore);
         document.getElementById('summary').innertHTML += incorrectByCategory.toString();
         console.log('Succsefully read exam data from database: ', results);
